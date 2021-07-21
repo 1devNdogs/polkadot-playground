a6ade7fdcedfed6cfbeab97c0ca18ea12954542f
1devNdogs <vsalgado.a@gmail.com>
2021-07-21 07:50:11 -0400

    loaders-fix

--- app-server.js
+++ app-server.js
@@ -1,31 +1,28 @@
 const express = require("express");
-var path = require('path');
+const path = require('path');
 
 const app = express();
-const distDir = path.join(__dirname, "public/");
 const PORT = process.env.PORT || 8080;
 
 function authentication(req, res, next) {
-  var authheader = req.headers.authorization;
+  let authheader = req.headers.authorization;
 
   if (!authheader) {
-    console.log(req.path);
-
-    var err = new Error('Not authenticated Error');
+    let err = new Error('Not authenticated Error');
     res.setHeader('WWW-Authenticate', 'Basic');
     err.status = 401;
     return next(err);
   }
 
-  var auth = new Buffer.from(authheader.split(' ')[1],
+  let auth = new Buffer.from(authheader.split(' ')[1],
     'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
+  let user = auth[0];
+  let pass = auth[1];
 
   if (user == 'admin' && pass == 'admin') {
     next();
   } else {
-    var err = new Error('You are not authenticated!');
+    let err = new Error('You are not authenticated!');
     res.setHeader('WWW-Authenticate', 'Basic');
     err.status = 401;
     return next(err);
